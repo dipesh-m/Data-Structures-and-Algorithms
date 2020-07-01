732541963b8f559b696bc8071eeef805962e1afe
dipesh-m <62656237+dipesh-m@users.noreply.github.com>
2020-07-01 22:12:01 +0530

    modified file

--- Online Submissions/GeeksForGeeks/String/permutations of a given string.cpp
+++ Online Submissions/GeeksForGeeks/String/permutations of a given string.cpp
@@ -45,3 +45,50 @@ int main()
 
     return 0;
 }
+
+
+
+//Without using inbuilt next_permutation() function
+
+
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+#include<algorithm>
+
+#define ll long long
+#define cases int t; cin>>t; for(int i=1; i<=t; i++)
+const int mod = 1000000007;
+const int MAX = 2e5;
+
+void solve()
+{
+    string s;
+    cin>>s;
+
+    sort(s.begin(), s.end());
+
+    bool cont=true;
+    while(cont)
+    {
+        cout<<s<<" ";
+        cont=next_permutation(s.begin(), s.end());
+    }
+
+    cout<<endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cases
+    {
+        solve();
+    }
+
+    return 0;
+}
+*/
