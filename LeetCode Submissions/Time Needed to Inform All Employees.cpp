894496d2ba8ad5fa131fc7f0b69696f8f2e2e6d6
dipesh-m <62656237+dipesh-m@users.noreply.github.com>
2021-05-16 00:05:05 +0530

    added files

---
+++ LeetCode Submissions/Time Needed to Inform All Employees.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime)
+    {
+        vector<vector<int>> vec(n);
+        for(int i=0; i<manager.size(); i++)
+        {
+            if(manager[i]!=-1)
+                vec.at(manager[i]).push_back(i);
+        }
+
+        int ans=0;
+        queue<pair<int,int>> q;
+        q.push(make_pair(headID,0));
+
+        while(!q.empty())
+        {
+            int sz=q.size();
+            for(int i=0; i<sz; i++)
+            {
+                pair<int,int> fr=q.front();
+                q.pop();
+
+                int curr=fr.second+informTime[fr.first];
+                ans=max(ans, curr);
+
+                for(int j=0; j<vec.at(fr.first).size(); j++)
+                    q.push(make_pair(vec.at(fr.first).at(j), curr));
+            }
+        }
+
+        return ans;
+    }
+};
